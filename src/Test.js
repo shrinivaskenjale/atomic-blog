a83130c96ed714e5547f6cc1adfd8cbb91adf251
Shrinivas Kenjale <sskenjale22@gmail.com>
2023-08-09 06:44:43 +0530

    Initial commit

---
+++ src/Test.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function SlowComponent() {
+  // If this is too slow on your machine, reduce the `length`
+  const words = Array.from({ length: 100_000 }, () => "WORD");
+  return (
+    <ul>
+      {words.map((word, i) => (
+        <li key={i}>
+          {i}: {word}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Test() {
+  return (
+    <Counter>
+      <SlowComponent />
+    </Counter>
+  );
+}
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      {children}
+    </div>
+  );
+}
